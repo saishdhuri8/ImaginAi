59897f679e8eb21671b5e675de9ee5326cabde71
Saish Dhuri <sdhuri392@gmail.com>
2025-04-26 02:21:49 +0530

    Update main.jsx

--- MPR-SEM4/client/src/main.jsx
+++ MPR-SEM4/client/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
-import { FireBaseProvider } from './firebase.jsx'
+import { FireBaseProvider } from './Firebase.jsx'
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js'
 
